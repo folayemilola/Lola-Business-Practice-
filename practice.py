12dc29539e0ceb1493286f5b503fb392ac3152fa
MissLola3050 <113481736+LolaFolayemi@users.noreply.github.com>
2024-02-14 19:12:33 -0600

    uploading the first practice

---
+++ practice.py
@@ -0,0 +1,20 @@
+# Difference between  lists and Tuples 
+
+#Tuple 
+person_a = ('name' , '18', 'Seneca', 'Chicago') # or without the poarenthesis 
+
+#List 
+Seneca = ['Jaime', 'Lola', 'Arai', 'Aria' 'Ethan']
+
+# to access a syntax in a tuple 
+person_a[2] # to access the third person in the syntax
+
+# python for loop
+my_name = "Lola"
+for letter in my_name:
+    print (letter)
+
+    # learning about enumerate 
+    groceries = ['roast beef', 'bread', 'water']
+    for index, item in enumerate(groceries, 1):
+        print (f'{index}. {item}')
